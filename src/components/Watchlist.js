f857ddfeb8eb7fb63517520e1f02336d9fbcdce3
nagachaitanya530 <nagachaitanya530@gmail.com>
2025-07-28 13:37:42 +0530

    Initial commit

---
+++ src/components/Watchlist.js
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
+
+export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWatchlist}
+              isWatchListed={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
